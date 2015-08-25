8e70ab4b7642648169dbe932a193dcc4f2113007
Jakub <truefear@gmail.com>
2015-08-25 11:44:51 +0200

    Create AbstractResultParser class.

--- omdbApi/OmdbResult.php
+++ omdbApi/OmdbResult.php
@@ -1,10 +1,10 @@
 <?php namespace Jtaurus\OmdbApi;
 
 use Jtaurus\OmdbApi\UnrecognizedDataStructureReturnedByApi;
-
+use Jtaurus\OmdbApi\AbstractResultParser;
 use Exception;
 
-class OmdbResult{
+class OmdbResult extends AbstractResultParser{
 
 	protected $apiResponseBlob;
 	protected $jsonData;
@@ -43,66 +43,7 @@ class OmdbResult{
 		$this->parseBasicData();
 	}
 
-	public function convertJsonToArray(){
-		$this->dataAsArray = $this->convertAllArrayKeysToLowerCase(json_decode($this->jsonData, true), CASE_LOWER);
-	}
 
-	public function convertXmlToArray(){
-		$loadedXml = simplexml_load_string($this->apiResponseBlob);
-		$asJson = json_encode($loadedXml);
-		$this->dataAsArray = $this->convertAllArrayKeysToLowerCase(json_decode($asJson, true));
-	}
-
-	protected function convertAllArrayKeysToLowerCase($array){
-		return array_map(function ($oneValue){
-			if(is_array($oneValue)){
-				$oneValue = $this->convertAllArrayKeysToLowerCase($oneValue);
-			}
-			return $oneValue;
-		}, array_change_key_case($array, CASE_LOWER));
-	}
-
-	// We are reshaping the array provided from XML conversion.
-	// Its structure will be the same as the one coming from JSON response.
-
-	public function reshapeArrayComingFromXml($array){
-		$newArray = array();
-		foreach($array as $oneKey => $oneValue){
-			if(is_array($oneValue)){
-				$newArray = array_merge($newArray, $this->reshapeArrayComingFromXml($oneValue));
-			}
-			else{
-				$newArray[$oneKey] = $oneValue;
-			}
-		}
-		return $newArray;
-	}
-
-	public function isJson($data){
-		if( json_decode($data) == NULL){
-			return false;
-		}
-		else{
-			return true;
-		}
-	}
-
-	public function isXml($data){
-			try{
-				if(simplexml_load_string($data) === false)
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-			catch(Exception $e)
-			{
-				return false;
-			}
-	}
 
 	public function getJson(){
 		return $this->jsonData;
