ad895da889ceba2b4be113f5090c9dec3b6665d3
Jakub <truefear@gmail.com>
2015-08-26 11:04:26 +0200

    Apply Allman indentation style to OmdbResultFactory.

--- omdbApi/OmdbResultFactory.php
+++ omdbApi/OmdbResultFactory.php
@@ -4,12 +4,12 @@ use Jtaurus\OmdbApi\CreateFromApiResponse;
 use Jtaurus\OmdbApi\OmdbResult;
 
 class OmdbResultFactory implements CreateFromApiResponse{
-
 	/*
 		@param - guzzlehttp response
 		@return - OmdbResult object
 	*/
-	public function createFromApiResponse($apiResponse){
+	public function createFromApiResponse($apiResponse)
+	{
 		return new OmdbResult((string) $apiResponse->getBody());
 	}
 }
