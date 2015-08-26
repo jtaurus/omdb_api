59ec0924994783f9430fefcfa3e4dc5cb3c48d24
Jakub <truefear@gmail.com>
2015-08-26 11:01:31 +0200

    Apply Allman indentation style to OmdbApi.

--- omdbApi/OmdbApi.php
+++ omdbApi/OmdbApi.php
@@ -9,36 +9,42 @@ class OmdbApi{
 	
 	protected $omdbQueryInstance;
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->omdbQueryInstance = new OmdbQuery;
 	}
 
 
-	public function byID($i, $length = "short", $return = "json"){
+	public function byID($i, $length = "short", $return = "json")
+	{
 		$queryUrl = QueryBuilder::create(array("i" => $i, "plot" => $length, "r" => $return));
 		$omdbResult = $this->omdbQueryInstance->runQuery($queryUrl, new OmdbResultFactory());
 		return $omdbResult->getMovieData();
 	}
 
-	public function byTitle($title, $length = "short", $return = "json"){
+	public function byTitle($title, $length = "short", $return = "json")
+	{
 		$queryUrl = QueryBuilder::create(array("t" => $title, "plot" => $length, "r" => $return));
 		$omdbResult = $this->omdbQueryInstance->runQuery($queryUrl, new OmdbResultFactory());
 		return $omdbResult->getMovieData();
 	}
 
-	public function byTitleYear($title, $year, $length = "short", $return = "json"){
+	public function byTitleYear($title, $year, $length = "short", $return = "json")
+	{
 		$queryUrl = QueryBuilder::create(array("t" => $title, "y" => $year, "plot" => $length, "r" => $return));
 		$omdbResult = $this->omdbQueryInstance->runQuery($queryUrl, new OmdbResultFactory());
 		return $omdbResult->getMovieData();
 	}
 
-	public function search($query, $return = "json"){
+	public function search($query, $return = "json")
+	{
 		$queryUrl = QueryBuilder::create(array("s" => $query, "r" => $return));
 		$searchParserInstance = $this->omdbQueryInstance->runQuery($queryUrl, new SearchParserFactory());
 		return $searchParserInstance->getSearchData();
 	}
 
-	public function getFirstFromSearch($query, $return = "json"){
+	public function getFirstFromSearch($query, $return = "json")
+	{
 		$queryUrl = QueryBuilder::create(array("s" => $query, "r" => $return));
 		$searchParserInstance = $this->omdbQueryInstance->runQuery($queryUrl, new SearchParserFactory);
 		return $searchParserInstance->getSearchData()->getSearchResultsArray()[0];
