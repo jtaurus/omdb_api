b2e0422ea11302a4a67150be72858e815a5ea733
Jakub <truefear@gmail.com>
2015-08-26 09:49:55 +0200

    Rename SearchParserFactory file.

---
+++ omdbApi/SearchParserFactory.php
@@ -0,0 +1,10 @@
+<?php namespace Jtaurus\OmdbApi;
+
+use Jtaurus\OmdbApi\CreateFromApiResponse;
+use Jtaurus\OmdbApi\SearchParser;
+
+class SearchParserFactory implements CreateFromApiResponse{
+	public function createFromApiResponse($apiResponse){
+		return new SearchParser((string) $apiResponse->getBody());
+	}
+}
