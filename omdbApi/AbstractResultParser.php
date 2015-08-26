c99c653c840b8e852d1f01b6ff0bee8b4d0d2cf2
Jakub <truefear@gmail.com>
2015-08-26 10:57:43 +0200

    Apply Allman indentation and adhere to PSR-2

--- omdbApi/AbstractResultParser.php
+++ omdbApi/AbstractResultParser.php
@@ -3,8 +3,7 @@
 use Jtaurus\OmdbApi\UnrecognizedDataStructureReturnedByApi;
 use Exception;
 
-abstract class AbstractResultParser
-{
+abstract class AbstractResultParser{
 	
 	protected $apiResponseBlob;
 	protected $jsonData;
@@ -15,38 +14,46 @@ abstract class AbstractResultParser
 	protected $responseSuccessful;
 	protected $errorMessage;
 
-	protected function handleApiResponse($apiResponseBlob){
+	protected function handleApiResponse($apiResponseBlob)
+	{
 		$this->apiResponseBlob = $apiResponseBlob;
 
-		if($this->isJson($apiResponseBlob)){
+		if($this->isJson($apiResponseBlob))
+		{
 			$this->jsonData = $apiResponseBlob;
 			$this->convertJsonToArray();
 		}
-
-		else if($this->isXml($apiResponseBlob)){
+		else if($this->isXml($apiResponseBlob))
+		{
 			$this->xmlData = $apiResponseBlob;
 			$this->convertXmlToArray();
 			$this->dataAsArray = $this->reshapeArrayComingFromXml($this->dataAsArray);
 		}
-		else{
+		else
+		{
 			throw new UnrecognizedDataStructureReturnedByApi("Api did not return JSON nor XML.");
 		}
 		$this->parseResponseMetaData();
 	}
 
-	public function convertJsonToArray(){
+	public function convertJsonToArray()
+	{
 		$this->dataAsArray = $this->convertAllArrayKeysToLowerCase(json_decode($this->jsonData, true), CASE_LOWER);
 	}
 
-	public function convertXmlToArray(){
+	public function convertXmlToArray()
+	{
 		$loadedXml = simplexml_load_string($this->apiResponseBlob);
 		$asJson = json_encode($loadedXml);
 		$this->dataAsArray = $this->convertAllArrayKeysToLowerCase(json_decode($asJson, true));
 	}
 
-	protected function convertAllArrayKeysToLowerCase($array){
-		return array_map(function ($oneValue){
-			if(is_array($oneValue)){
+	protected function convertAllArrayKeysToLowerCase($array)
+	{
+		return array_map(function ($oneValue)
+			{
+			if(is_array($oneValue))
+			{
 				$oneValue = $this->convertAllArrayKeysToLowerCase($oneValue);
 			}
 			return $oneValue;
@@ -56,62 +63,76 @@ abstract class AbstractResultParser
 	// We are reshaping the array provided from XML conversion.
 	// Its structure will be the same as the one coming from JSON response.
 
-	public function reshapeArrayComingFromXml($array){
+	public function reshapeArrayComingFromXml($array)
+	{
 		$newArray = array();
-		foreach($array as $oneKey => $oneValue){
-			if(is_array($oneValue)){
+		foreach($array as $oneKey => $oneValue)
+		{
+			if(is_array($oneValue))
+			{
 				$newArray = array_merge($newArray, $this->reshapeArrayComingFromXml($oneValue));
 			}
-			else{
+			else
+			{
 				$newArray[$oneKey] = $oneValue;
 			}
 		}
 		return $newArray;
 	}
 
-	public function isJson($data){
-		if( json_decode($data) == NULL){
+	public function isJson($data)
+	{
+		if( json_decode($data) == NULL)
+		{
 			return false;
 		}
-		else{
+		else
+		{
 			return true;
 		}
 	}
 
-	public function isXml($data){
-			try{
-				if(simplexml_load_string($data) === false)
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-			catch(Exception $e)
+	public function isXml($data)
+	{
+		try
+		{
+			if(simplexml_load_string($data) === false)
 			{
 				return false;
 			}
+				else
+			{
+				return true;
+			}
+		}
+		catch(Exception $e)
+		{
+				return false;
+		}
 	}
 
-	public function getJson(){
+	public function getJson()
+	{
 		return $this->jsonData;
 	}
 
-	public function getAssocArray(){
+	public function getAssocArray()
+	{
 		return $this->dataAsArray;
 	}
 
-	public function getErrorMessage(){
+	public function getErrorMessage()
+	{
 		return $this->errorMessage;
 	}
 
-	public function getResponseStatus(){
+	public function getResponseStatus()
+	{
 		return $this->responseSuccessful;
 	}
 
-	protected function parseResponseMetaData(){
+	protected function parseResponseMetaData()
+	{
 		$this->responseSuccessful = (isset($this->dataAsArray["response"]) && $this->dataAsArray["response"] == "True") ? true : false;
 		$this->errorMessage = (isset($this->dataAsArray["error"])) ? $this->dataAsArray["error"] : null;
 	}
