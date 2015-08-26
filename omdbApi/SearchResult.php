13fb1a7a63a5714525532891dddc05195ea1caa8
Jakub <truefear@gmail.com>
2015-08-26 09:50:45 +0200

    Create SearchResult class stub.

--- omdbApi/SearchResult.php
+++ omdbApi/SearchResult.php
@@ -1,23 +1,10 @@
 <?php namespace Jtaurus\OmdbApi;
 
 use Exception;
-use Jtaurus\OmdbApi\AbstractResultParser;
 
-class SearchParser extends AbstractResultParser
-
+class SearchResult
 {
-	protected $foundResults;
-
-	public function __construct($apiResponseBlob){
-		$this->handleApiResponse($apiResponseBlob);
-		$this->parseResponseIntoAnArrayOfMovies();
-	}
-
-	protected function parseResponseIntoAnArrayOfMovies(){
-		$this->foundResults = $this->dataAsArray["search"];
-	}
-
-	public function getArrayOfFoundResults(){
-		return $this->foundResults;
+	public function __construct(){
+		
 	}
 }
